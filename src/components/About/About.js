11e8f0ba019eccd4716dfd371afe408882a62023
emancho <emancho@umd.edu>
2024-10-16 23:01:32 -0400

    added portfolio picture and implemented feature to change the picture based on light/dark mode usage

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,13 @@
+import { useContext } from 'react'
 import GitHubIcon from '@material-ui/icons/GitHub'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import { ThemeContext } from '../../contexts/theme'
 import { about } from '../../portfolio'
 import './About.css'
 
 const About = () => {
   const { name, role, description, subDescription, resume, social } = about
+  const [{ themeName }] = useContext(ThemeContext)
 
   return (
     <div className='about center'>
@@ -16,7 +19,11 @@ const About = () => {
 
       {/* The section for the image */}
       <div className="card-with-border">
-            <h2>This is a card with a solid border!</h2>
+            {
+              themeName === 'dark' ? 
+              <img src='https://pubaccessbucket.s3.amazonaws.com/images/headshot-dark.jpg' alt='Header in black and white'/> : 
+              <img src='https://pubaccessbucket.s3.amazonaws.com/images/headshot-light.PNG' alt='Header in color'/>
+            }
       </div>
 
       {role && <h2 className='about__role'>A {role}.</h2>}
