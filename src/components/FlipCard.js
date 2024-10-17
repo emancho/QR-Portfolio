ab91db2dac72397b4fe89cc7f31788f9c986f91d
emancho <emancho@umd.edu>
2024-10-17 13:44:23 -0400

    changed the text for the back card flip. At this point, app is deployable

--- src/components/FlipCard.js
+++ src/components/FlipCard.js
@@ -19,7 +19,7 @@ function FlipCard({ front = null, back = null }) {
             <div className="card-side back">
                 <div className="card-content">{back || 'BACK CARD'}</div>
                 <div className="card-button-container">
-                    <button className="flip-button" type="button" onClick={handleFlip}>More</button>
+                    <button className="flip-button" type="button" onClick={handleFlip}>Back</button>
                 </div>
             </div>
         </div>
